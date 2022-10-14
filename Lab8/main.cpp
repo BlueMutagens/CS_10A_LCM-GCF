ac91ee0b44c2d741b3482353c61ef31e9c018d8c
Sean <sdbel@comcast.net>
2022-10-13 18:02:36 -0700

    Initial Commit

---
+++ Lab8/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab8
+//
+//  Created by Sean Belingheri on 10/13/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
